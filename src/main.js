b2eb71826da82405c845d7f50e080ed09f45cac3
Christopher Huber <CHuber42.work@gmail.com>
2020-04-24 12:40:15 -0700

    clean up code w/spacing and readability

--- src/main.js
+++ src/main.js
@@ -3,12 +3,12 @@ import 'bootstrap';
 import 'bootstrap/dist/css/bootstrap.min.css';
 import './styles.css';
 import { getRates } from "./fetch.js";
-import { processResult } from "./resultsFinder.js"
+import { processResult } from "./resultsFinder.js";
 
 
 $(document).ready(function() {
 
-    $("#submit").click(async function() {
+  $("#submit").click(async function() {
     let userDollars = $("#usd").val();
     let targetCurrency = $("#targetCurrency").val();
     let rates = await getRates();
@@ -18,9 +18,11 @@ $(document).ready(function() {
       $("#results_presentation").text(`${finalResult} ${targetCurrency}`);
     }
     else {
-      $("#results_presentation").text("I'm sorry, something went wrong. You may not have input a dollar value, or chosen a target currency that does not exist, or the API call may have failed. Check the console for details.");
-      console.log(`Dollars Input: ${userDollars}, Currency in list: ${rates[targetCurrency]}`)
+      $("#results_presentation").text(`I'm sorry, something went wrong. You may not have input a dollar value, 
+                                        or chosen a target currency that does not exist, or the API call may have failed. 
+                                        Check the console for details.`);
+      console.log(`Dollars Input: ${userDollars}, Currency in list: ${rates[targetCurrency]}`);
     }
-  })
-})
+  });
+});
 
