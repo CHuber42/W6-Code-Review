ca1f041197d0a3ec3dcf1d4775f3949d9b2ef5c7
Christopher Huber <CHuber42.work@gmail.com>
2020-04-24 12:24:52 -0700

    add if/else to main that checks inputs (dollars and target currency) as valid, and outputs error if not

--- src/main.js
+++ src/main.js
@@ -7,16 +7,19 @@ import { processResult } from "./resultsFinder.js"
 
 
 $(document).ready(function() {
-  $("#submit").click(async function() {
+
+    $("#submit").click(async function() {
     let userDollars = $("#usd").val();
     let targetCurrency = $("#targetCurrency").val();
     let rates = await getRates();
-    let finalResult = processResult(userDollars, targetCurrency, rates);
-    
-    $("#results_presentation").text(`${finalResult} ${targetCurrency}`);
-    
+
+    if (rates[targetCurrency] && userDollars > 0) {
+      let finalResult = processResult(userDollars, targetCurrency, rates);
+      $("#results_presentation").text(`${finalResult} ${targetCurrency}`);
+    }
+    else {
+      $("#results_presentation").text("I'm sorry, your input was invalid. You may not have input a dollar value, or chosen a target currency that does not exist.");
+    }
   })
-
-
 })
 
